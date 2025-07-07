d3eea56ccf0039587094964d93927d195731b451
Phillip Rearick <psrearick@gmail.com>
2025-07-07 16:01:14 -0400

    wip

--- rector.php
+++ rector.php
@@ -2,16 +2,16 @@
 
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
-use RectorLaravel\Set\LaravelLevelSetList;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use RectorLaravel\Set\LaravelLevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/app',
-        __DIR__ . '/config',
-        __DIR__ . '/database',
-        __DIR__ . '/routes',
-        __DIR__ . '/tests',
+        __DIR__.'/app',
+        __DIR__.'/config',
+        __DIR__.'/database',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
     ]);
 
     // PHP version upgrade
@@ -24,8 +24,8 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(AddVoidReturnTypeWhereNoReturnRector::class);
 
     $rectorConfig->skip([
-        __DIR__ . '/vendor',
-        __DIR__ . '/storage',
-        __DIR__ . '/bootstrap/cache',
+        __DIR__.'/vendor',
+        __DIR__.'/storage',
+        __DIR__.'/bootstrap/cache',
     ]);
 };
