452df8f0a05dd20894e9e9f6f851a34e01e09d81
Phillip Rearick <psrearick@gmail.com>
2025-07-07 15:30:10 -0400

    start adding code quality tools

---
+++ rector.php
@@ -0,0 +1,31 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use RectorLaravel\Set\LaravelLevelSetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/app',
+        __DIR__ . '/config',
+        __DIR__ . '/database',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+    ]);
+
+    // PHP version upgrade
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_84,
+        LaravelLevelSetList::UP_TO_LARAVEL_120,
+    ]);
+
+    // Custom rules
+    $rectorConfig->rule(AddVoidReturnTypeWhereNoReturnRector::class);
+
+    $rectorConfig->skip([
+        __DIR__ . '/vendor',
+        __DIR__ . '/storage',
+        __DIR__ . '/bootstrap/cache',
+    ]);
+};
